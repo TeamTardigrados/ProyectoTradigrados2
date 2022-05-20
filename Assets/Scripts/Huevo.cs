017283b3341d3c9b26ed847b9bf42764844c4af9
Valentina <valentina.francoa@upb.edu.co>
2022-05-20 06:34:30 -0500

    v

--- Assets/Scripts/Huevo.cs
+++ Assets/Scripts/Huevo.cs
@@ -8,50 +8,38 @@ public class Huevo : MonoBehaviour
     [SerializeField] private Animator animator = null;
     [SerializeField] private GameObject recogerP = null;
     [SerializeField] private GameObject turbinaCohete = null;
-    [SerializeField] private Texto dialoguePanel = null;
+    int Contador = 0;
     [SerializeField] Button BtnContinuar;
-    [SerializeField] private float waitTimeBrokenEgg = 2f;
-    private int counter = 0;
-    private SoundManager soundManager;
-
     void Start()
     {
         BtnContinuar.interactable = false;
     }
-    private void Awake()
-    {
-        soundManager = FindObjectOfType<SoundManager>();
-    }
     private void OnMouseDown()
     {
-        counter++;
-        switch (counter)
-        {
-            case 1:
-                animator.SetTrigger("isOpen1");
-                soundManager.SeleccionAudio(0, 0.5f);
-                break;
+            Contador++;
 
-            case 2:
-                animator.SetTrigger("isOpen2");
-                soundManager.SeleccionAudio(0, 0.5f);
-                break;
+            switch (Contador)
+            {
+                case 1:
+                    animator.SetTrigger("isOpen1");
+                    break;
 
-            case 3:
-                animator.SetTrigger("isOpen3");
-                soundManager.SeleccionAudio(7, 0.5f);
-                StartCoroutine(WaitThenLoad());
-                break;
-        }
+                case 2:
+                    animator.SetTrigger("isOpen2");
+                    break;
+
+                case 3:
+                    animator.SetTrigger("isOpen3");
+                    StartCoroutine(WaitThenLoad());
+                    BtnContinuar.interactable = true;
+                    break;
+            }
     }
-
+   
     private IEnumerator WaitThenLoad()
     {
-        yield return new WaitForSecondsRealtime(waitTimeBrokenEgg);
+        yield return new WaitForSecondsRealtime(2f);
         recogerP.SetActive(true);
-        turbinaCohete.SetActive(true);
-        dialoguePanel.LineTemp = 2;
-        dialoguePanel.StartDialogue();
-        BtnContinuar.interactable = true;
+       turbinaCohete.SetActive(true);
     }
 }
